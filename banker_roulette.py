8a5d3ce30a61bab0881998a8bd7a393b4d5429a6
Mara Green <maragreen42@gmail.com>
2023-04-16 18:25:40 -0700

    treasure_map excersise completed

--- banker_roulette.py
+++ banker_roulette.py
@@ -1,16 +1,30 @@
 # Import the random module here
-import random
-# Split string method
-names_string = input("Give me everybody's names, separated by a comma. ")
-names = names_string.split(", ")
-# 🚨 Don't change the code above 👆
+# import random
+# # Split string method
+# names_string = input("Give me everybody's names, separated by a comma. ")
+# names = names_string.split(", ")
+# # 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
+# #Write your code below this line 👇
 
-length_of_list = len(names)
+# length_of_list = len(names)
 
-paying_for_meal = random.randint(0, length_of_list)
+# paying_for_meal = random.randint(0, length_of_list)
 
-name_of_person_paying = names[paying_for_meal]
+# name_of_person_paying = names[paying_for_meal]
 
-print(f"{name_of_person_paying} is going to buy the meal today!")
+# print(f"{name_of_person_paying} is going to buy the meal today!")
+
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+ 
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen[0])
+print(dirty_dozen[1])
+
+print(dirty_dozen[1][2])
+print(dirty_dozen[1][3])
+ 
+print(dirty_dozen[1][1])
