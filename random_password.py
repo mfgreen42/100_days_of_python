f264a9faf6152d4e44c639a55ad3d924541fd4a3
Mara Green <maragreen42@gmail.com>
2023-04-21 21:52:25 -0700

    random_password excersise completed

---
+++ random_password.py
@@ -0,0 +1,25 @@
+#starter code
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+letter_choice = random.choices(letters, k = nr_letters)
+number_choice = random.choices(numbers, k = nr_numbers)
+symbol_choice = random.choices(symbols, k = nr_symbols)
+
+char = nr_letters + nr_numbers + nr_symbols
+
+# compontents = [letter_choice, number_choice, symbol_choice]
+compontents = letter_choice + number_choice + symbol_choice
+
+random.shuffle(compontents)
+
+for c in compontents:
+    print(c, end ='')
