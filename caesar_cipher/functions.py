5dd468738b7279dbc08087d2a4827892c580c6b0
Mara Green <maragreen42@gmail.com>
2023-05-07 11:44:41 -0700

    prime number finder completed

---
+++ caesar_cipher/functions.py
@@ -0,0 +1,12 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+def greet(something):
+    print(f"hello {something}")
+    print(f"how is your day?")
+    print(f"today is great")
+
+
+greet('Mara')
