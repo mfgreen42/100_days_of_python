d82898be35c81f436d88c45e38fafe2dc3313d04
Mara Green <maragreen42@gmail.com>
2023-07-09 21:11:16 -0700

    pick_winner funtion completed

--- Higher or Lower/higher_or_lower.py
+++ Higher or Lower/higher_or_lower.py
@@ -27,9 +27,10 @@ print(logo)
 play = True
 
 correct = 0
-
+compare_again = True
 entry_a = {}
 entry_b = {}
+winning_entry = ""
 def draw_entries():
     global entry_a
     global entry_b
@@ -46,17 +47,43 @@ def print_entries(entry_a, entry_b):
     print(f"Compare B: {entry_b.get('name')}, a {entry_b.get('description')}, from {entry_b.get('country')}")
 
 def compare_entries(entry_a, entry_b):
-    if 
+    global winning_entry
+    global user_choice
+    if entry_a['follower_count'] > entry_b['follower_count']:
+        winning_entry = entry_a
+    elif entry_a['follower_count'] < entry_b['follower_count']:
+        winning_entry = entry_b
+    user_choice = input("Who has more followers?: A or B ")
+    user_choice = user_choice.upper()
+    if user_choice == "A":
+        user_choice == entry_a
+    elif user_choice == "B":
+        user_choice = entry_b 
+    return winning_entry, user_choice
+
+def pick_winner(winning_entry, user_choice, entry_a, entry_b):
+    global compare_again
+    if user_choice == winning_entry:
+        print("That is correct!")
+        entry_a = winning_entry
+        return entry_a
+    else:
+        print("I'm sorry, that is incorrect")
+        compare_again = False
+
+# def second_round():
 
     
 
-while play:
 
-    def game_play():
-        draw_entries()
-        print_entries(entry_a, entry_b)
+def game_play():
+    draw_entries()
+    print_entries(entry_a, entry_b)
+    while compare_again:
         compare_entries(entry_a, entry_b)
+        pick_winner(winning_entry, user_choice, entry_a, entry_b)
 
-    game_play()
+    play_again()
+game_play()
 
 
